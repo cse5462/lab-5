a95dc88560d339ab1c6d824d93f3991dc8ca1d4d
Conner Graham <59581008+cgraham1092@users.noreply.github.com>
2021-03-05 04:01:18 -0500

    Fixed accidental change of game_over

--- tictactoeServer.c
+++ tictactoeServer.c
@@ -103,7 +103,7 @@ void print_board(const struct TTT_Game *game);
 int validate_move(int choice, const struct TTT_Game *game);
 int send_p1_move(int sd, struct TTT_Game *game);
 void free_game(struct TTT_Game *game);
-int check_game_over(struct TTT_Game *game);
+int game_over(struct TTT_Game *game);
 void tictactoe(int sd);
 
 /**
@@ -464,7 +464,7 @@ void move(int sd, const struct sockaddr_in *playerAddr, const struct Buffer *dat
         if (validate_move(move, game)) {
             /* Update the board (for Player 2) and check if someone won */
             game->board[move-1] = P2_MARK;
-            if (check_game_over(game)) return;
+            if (game_over(game)) return;
             /* If nobody won, change turns and make a move to send to the remote player */
             game->player = 1;
             if ((move = send_p1_move(sd, game)) == ERROR_CODE) {
@@ -473,7 +473,7 @@ void move(int sd, const struct sockaddr_in *playerAddr, const struct Buffer *dat
             }
             /* Update the board (for Player 1) and check if someone won */
             game->board[move-1] = P1_MARK;
-            if (check_game_over(game)) return;
+            if (game_over(game)) return;
             /* If nobody won, change turns and print the board after the exchange */
             game->player = 2;
             print_board(game);
@@ -717,7 +717,7 @@ void free_game(struct TTT_Game *game) {
  * @param game The current game of TicTacToe being played.
  * @return True if the game has ended, false otherwise. 
  */
-int check_game_over(struct TTT_Game *game) {
+int game_over(struct TTT_Game *game) {
     /* Check if somebody won the game */
     if (check_win(game) != 0) {
         /* Print final game board and winning player */
